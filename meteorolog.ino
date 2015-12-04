ef891bc4122cf10af67acf8c37aa8be51f4fc44d
Alejandro Druetta <aledruetta@gmail.com>
2015-12-04 14:41:13 -0200

    ds18b20 improvements

--- meteorolog.ino
+++ meteorolog.ino
@@ -10,13 +10,19 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 10
+// Sensor input pin
+#define DATA_PIN 10
+// How many bits to use for temperature values: 9, 10, 11 or 12
+#define SENSOR_RESOLUTION 9
+// Index of sensors connected to data pin, default: 0
+#define SENSOR_INDEX 0
 
 // DS18S20 Temperature chip i/o on pin 10
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(DATA_PIN);
 
 // Tell Dallas Temperature Library to use oneWire library
 DallasTemperature sensors(&oneWire);
+DeviceAddress sensorDeviceAddress;
 
 void setup(void) {
   Serial.begin(9600);
@@ -27,10 +33,13 @@ void setup(void) {
 
   // Start up DallasTemperature library
   sensors.begin();
+  sensors.getAddress(sensorDeviceAddress, 0);
+  sensors.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
 }
 
 void loop(void) {
   temp_ds18b20();
+  delay(5000);      // Read frequency
 }
 
 void temp_ds18b20(void) {
@@ -40,6 +49,6 @@ void temp_ds18b20(void) {
   Serial.println("DONE");
 
   Serial.print("Device 1 (index 0) = ");
-  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print(sensors.getTempCByIndex(SENSOR_INDEX));
   Serial.println(" Degrees C");
 }
